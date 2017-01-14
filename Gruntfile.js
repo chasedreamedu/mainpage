45973211cecbf1bfafc072cc3225a0439ca5f94f
Yan <yanche.belongs@gmail.com>
2017-01-14 11:10:56 -0800

    gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+﻿
+module.exports = function (grunt) {
+    grunt.initConfig({
+        exec: {
+            jadeCompile: {
+                cmd: 'node make.js',
+            }
+        },
+        copy: {
+            libs: {
+                files: [{
+                    expand: true,
+                    cwd: 'bower_components/font-awesome',
+                    src: ['css/*', 'fonts/*'],
+                    dest: 'static/font-awesome',
+                    filter: 'isFile'
+                }, {
+                    src: 'bower_components/jquery/dist/jquery.min.js',
+                    dest: 'static/jquery.min.js'
+                }, {
+                    expand: true,
+                    cwd: 'bower_components/bootstrap/dist',
+                    src: ['css/*', 'fonts/*', 'js/*'],
+                    dest: 'static/bootstrap',
+                    filter: 'isFile'
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.registerTask('default', ['exec', 'copy']);
+};
